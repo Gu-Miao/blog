faa46f61fc4578fcde061652c5dc2d3ecc1ac190
Gu-Miao <1450206741@qq.com>
2022-07-22 18:16:02 +0800

    Add solid integration and add PostList component

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config'
 import sitemap from '@astrojs/sitemap'
+import solidjs from '@astrojs/solid-js'
 
 export default defineConfig({
   site: 'https://guangwublog.vercel.app',
-  integrations: [sitemap()],
+  integrations: [sitemap(), solidjs()],
 })
---
+++ src/components/PostList/index.tsx
@@ -0,0 +1,49 @@
+import { Component, createSignal, For } from 'solid-js'
+import dayjs from 'dayjs'
+import { MarkdownInstance } from 'astro'
+import { Frontmatter } from '@/common/types'
+import './index.less'
+
+interface SearchProps {
+  posts: MarkdownInstance<Frontmatter>[]
+}
+
+const Search: Component<SearchProps> = props => {
+  const [value, setValue] = createSignal('')
+  const filterdPosts = () =>
+    props.posts.filter(post => post.frontmatter.title.toLowerCase().includes(value().toLowerCase()))
+
+  return (
+    <div class="post-list">
+      <input
+        type="text"
+        value={value()}
+        onInput={e => {
+          console.log(12123)
+          setValue(e.currentTarget.value)
+        }}
+        placeholder="在此搜索..."
+      />
+      <ul>
+        <For each={filterdPosts()}>
+          {post => (
+            <li>
+              <h3>
+                <a href={post.url}>{post.frontmatter.title}</a>
+              </h3>
+              <p>{post.frontmatter.description}</p>
+              <p>
+                {post.frontmatter.tags.map(tag => (
+                  <span class="tag">{tag}</span>
+                ))}
+              </p>
+              <p class="time">{dayjs(post.frontmatter.createdAt).format('YYYY年M月D日')}</p>
+            </li>
+          )}
+        </For>
+      </ul>
+    </div>
+  )
+}
+
+export default Search
