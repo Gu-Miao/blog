94f2de39595b1ebaedca99041327680650f6f52c
Gu-Miao <1450206741@qq.com>
2022-08-10 11:21:33 +0800

    Update astro to 1.0
    1. Update packages
    2. Update rss page

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,9 +1,13 @@
 import rss from '@astrojs/rss'
-//@ts-ignore
 import dayjs from 'dayjs'
 import { TITLE, DESCRIPTION, LANGUAGE } from '@/common/constants'
+import { MarkdownInstance } from 'astro'
+import { Frontmatter } from '@/common/types'
 
-const postImportResult = import.meta.globEager('./posts/**/*.md')
+const postImportResult = import.meta.glob('./posts/**/*.md', { eager: true }) as Record<
+  string,
+  MarkdownInstance<Frontmatter>
+>
 const posts = Object.values(postImportResult)
 
 export const get = () =>
@@ -11,11 +15,13 @@ export const get = () =>
     title: TITLE,
     description: DESCRIPTION,
     site: import.meta.env.SITE,
-    items: posts.map(post => ({
-      link: post.url,
-      title: post.frontmatter.title,
-      description: post.frontmatter.description,
-      pubDate: dayjs(post.frontmatter.createdAt).toDate(),
-    })),
+    items: posts
+      .map(post => ({
+        link: post.url,
+        title: post.frontmatter.title,
+        description: post.frontmatter.abstract,
+        pubDate: dayjs(post.frontmatter.updatedAt).toDate(),
+      }))
+      .sort((p1, p2) => dayjs(p2.pubDate).unix() - dayjs(p1.pubDate).unix()),
     customData: `<language>${LANGUAGE}</language>`,
   })
